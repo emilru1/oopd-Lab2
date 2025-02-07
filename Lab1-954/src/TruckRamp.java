d9d06c302172c1d8caec51969cd11aafe4f60790
emilru1 <emilru@chalmers.se>
2025-02-07 16:09:21 +0100

    Update TruckRamp.java

--- Lab1-954/src/TruckRamp.java
+++ Lab1-954/src/TruckRamp.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public interface TruckRamp {
     void setRampDown();
     void setRampUp();
+    void startLoadMode();
+    void exitLoadMode();
     void loadCar(PersonalCar personalCar);
-    void unloadCar(PersonalCar personalCar);
-
+    void unloadCar();
+    ArrayList<String> getLoadedCars();
 }
